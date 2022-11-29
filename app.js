5dc9c5e00b43a890a45d1c13a71d22cdfe0427d6
Mukesh prajapati <94641465+Mukeshkingstar@users.noreply.github.com>
2022-11-29 23:16:07 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+function o(){
+    document. getElementById('side').style.display = "block"
+  }
+  function c(){
+  
+    document. getElementById('side').style.display = "none"
+  }
+  function s(){
+    var a = document.getElementById("code").value;
+  if (a=='mukeshkingstar') {
+    window.open("https://followeran.com/en/free-instagram-followers/")
+  } else {
+    alert('WRONG CODE TRY AGAIN')
+  }
+  }
